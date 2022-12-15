432a2539092d845972c63dbb3f357f8bb25a24e1
Cherry342 <aokagejasmin@brooklynsteamcenter.org>
2022-12-15 10:54:31 -0500

    add main.py

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 file= open("NASA_access_log_Jul95")
 
+from _collections_abc import defaultdict
+items=file(list)
 ip_adresses = []
 try:
     for line in file:
-     ip_adresses.append(line.split(" ")[0])
+        print(file)
 
+    ip_adresses.append(line.split(" ")[0])
     print(line.split(" ")[0])
-      
+    from _collections_abc,
+    print(file(list))
+
+
 except:
-      print("uh-oh")
+    print("uh-oh")
+    pass
+
 
