e04ca6bbedb2ed8f925be6a86a4bf4c4d4b43a52
Cherry342 <aokagejasmin@brooklynsteamcenter.org>
2022-12-19 10:57:34 -0500

    add pratice.py

--- pratice.py
+++ pratice.py
@@ -18,13 +18,13 @@
 #plt.show()
 #plt.title("Triangle")
 
-import numpy as np
-import matplotlib.pyplot as plt
+#import numpy as np
+#import matplotlib.pyplot as plt
 
-x = np.array([80, 85, 90, 95, 100, 105, 110, 115, 120, 125])
-y = np.array([240, 250, 260, 270, 280, 290, 300, 310, 320, 330])
-plt.plot(x, y)
+#x = np.array([80, 85, 90, 95, 100, 105, 110, 115, 120, 125])
+#y = np.array([240, 250, 260, 270, 280, 290, 300, 310, 320, 330])
+#plt.plot(x, y)
 
-plt.grid(color = 'green', linestyle = '--', linewidth = 0.5)
+#plt.grid(color = 'green', linestyle = '--', linewidth = 0.5)
 
-plt.show()
+#plt.show()
