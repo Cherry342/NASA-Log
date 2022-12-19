6f7b799a16312270f1fb7311a0f11c48dd11a421
Cherry342 <aokagejasmin@brooklynsteamcenter.org>
2022-12-19 10:52:13 -0500

    initial commit

---
+++ pratice.py
@@ -0,0 +1,30 @@
+# Import things
+#import matplotlib.pyplot as plt
+#import numpy as np
+#import matplotlib as mpl
+
+#xpoints = list(range(0,10)) 
+#ypoints= list(range(-10,0))
+#plt.plot (xpoints,ypoints)
+#plt.show()
+# Import things
+#import numpy as np
+#import matplotlib.pyplot as plt
+
+#x = np.array([80, 85, 90, 95, 100, 105, 110, 115, 120, 125])
+#y = np.array([240, 250, 260, 270, 280, 290, 300, 310, 320, 330])
+#plt.plot(x, y)
+#plt.grid()
+#plt.show()
+#plt.title("Triangle")
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.array([80, 85, 90, 95, 100, 105, 110, 115, 120, 125])
+y = np.array([240, 250, 260, 270, 280, 290, 300, 310, 320, 330])
+plt.plot(x, y)
+
+plt.grid(color = 'green', linestyle = '--', linewidth = 0.5)
+
+plt.show()
