474336f761fb798676c52b7558c3f2afc5d6cbca
Cherry342 <aokagejasmin@brooklynsteamcenter.org>
2023-01-05 10:37:43 -0500

    add main.py

--- main.py
+++ main.py
@@ -1,30 +1,25 @@
 file= open("NASA_access_log_Jul95")
 
-from _collections_abc import defaultdict
+
+#from _collections_abc import defaultdict
 from collections import Counter
-items=file(list)
 ip_adresses = []
 try:
     for line in file:
-        print(file)
-
-    
-    
-    ip_adresses.append(line.split(" ")[0])
-    
-    print(line.split(" ")[0])
-   # from _collections_abc:
-    #print(file(list))
+       ip_adresses.append(line.split(" ")[0])
 
 
 except:
-    print("uh-oh")
-    pass
-import matplotlib.pyplot as plt
-count = Counter(ip_adresses)
-print(ip_adresses)
-print(ip_adresses.keys())
-print(ip_adresses.values())
+  print("uh-oh")
 
-plt.bar(ip_adresses.keys(),ip_adresses.values())
+import matplotlib.pyplot as plt
+
+count = Counter(ip_adresses)
+bar_labels = ['pink', 'blue', 'orange', 'purple']
+bar_colors = ['tab:pink', 'tab:blue', 'tab:orange', 'tab:purple']
+filtered_count = {k:v for (k,v) in count.items() if v>550}
+plt.set_xlabel('Berries')
+plt.bar(filtered_count.keys(), filtered_count.values(), color=bar_colors, )
 plt.show()
+
+# print({k:v for (k,v) in names.items if k%2==()})
